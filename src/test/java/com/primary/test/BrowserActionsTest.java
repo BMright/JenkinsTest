36cbcc68ae1a5c24c4a34f418f151384d15d4d9f
BMright <1361585641@qq.com>
2020-05-21 12:10:31 +0800

    添加断言失败

--- src/test/java/com/primary/test/BrowserActionsTest.java
+++ src/test/java/com/primary/test/BrowserActionsTest.java
@@ -3,6 +3,7 @@ package com.primary.test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -90,6 +91,7 @@ public class BrowserActionsTest {
         // 得到当前窗口URL
         String url = webDriver.getCurrentUrl();
         System.out.println("当前窗口url为：" + url);
+        Assert.assertEquals(1,2);
     }
 
     @Test
